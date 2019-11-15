40f52ef4dcad235b12e075e108e483d81be72718
priyamvada <pranshulpandey5@gmail.com>
2019-11-15 23:59:47 +0000

    Updates

---
+++ precache-manifest.675863a736ba5d21d0ac9a26a298f397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be47f12e807c31bb21568aa16ff99a6e",
+    "url": "/facerecognitionweb_clarifai/index.html"
+  },
+  {
+    "revision": "c15378bd7b7c03b052f3",
+    "url": "/facerecognitionweb_clarifai/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e6eb01c60208dab944a8",
+    "url": "/facerecognitionweb_clarifai/static/css/main.a1df48ed.chunk.css"
+  },
+  {
+    "revision": "c15378bd7b7c03b052f3",
+    "url": "/facerecognitionweb_clarifai/static/js/2.7473daae.chunk.js"
+  },
+  {
+    "revision": "e6eb01c60208dab944a8",
+    "url": "/facerecognitionweb_clarifai/static/js/main.8c596f7f.chunk.js"
+  },
+  {
+    "revision": "caf70519cb0cfc8d8336",
+    "url": "/facerecognitionweb_clarifai/static/js/runtime-main.41722c5b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/facerecognitionweb_clarifai/static/media/SEGA.12a3db8b.woff"
+  }
+]);
