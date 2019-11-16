da30fa0f97f5b20133cb698bb92b3814dd7fb796
priyamvada <pranshulpandey5@gmail.com>
2019-11-16 11:30:50 +0000

    changes made after few eliminating web cam feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navigation from './container/Navigation/Navigation';
 import FaceReadingPath from './container/FaceReadingPath/FaceReadingPath';
 import Content from './container/Content/Content';
-import WebcamCapture from './container/Camera/WebcamCapture';
 import Particles from 'react-particles-js';
 import {Particle_constant} from './Particle_constant';
 import Clarifai from 'clarifai'
@@ -59,7 +58,6 @@ class App extends Component{
         <Navigation />
         <FaceReadingPath OnDetect={this.OnDetect} OnTextEnter={this.OnTextEnter} />
         <Content coordinate={this.state.Array_box} img_path={this.state.image_url} wide={this.state.wide} heigh={this.state.heigh}/> 
-        <WebcamCapture />
     </div>
   );
 }
